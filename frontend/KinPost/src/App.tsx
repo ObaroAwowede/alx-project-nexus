4d4fed0d663dc213e44ae046f97c2e206119c1da
Obaro Awowede <akpobaroawowede@gmail.com>
2025-08-05 11:28:09 +0100

    Created a layout file seperately for the forms, wrapped the form components inside this layout

--- frontend/KinPost/src/App.tsx
+++ frontend/KinPost/src/App.tsx
@@ -4,14 +4,17 @@ import SignInForm from "./components/SignInForm";
 import { Home } from "./navigation/pages";
 import SignUpForm from "./components/SignUpForm";
 import PageLayout from "./navigation/PageLayout";
+import FormLayout from "./components/FormLayout";
 
 function App() {
   return (
     <main className="flex h-screen">
       <Route element={<PageLayout />}>
+        <Route index element={<Home />} />
+      </Route>
+      <Route element= {<FormLayout/>}>
         <Route path="/sign-in" element={<SignInForm />} />
         <Route path="/sign-up" element={<SignUpForm />} />
-        <Route index element={<Home />} />
       </Route>
     </main>
   );
---
+++ frontend/KinPost/src/components/FormLayout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FormLayout = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default FormLayout
