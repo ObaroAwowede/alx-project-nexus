5877db7e45674c3ad0c37ed73f0bd4236e38076d
Obaro Awowede <akpobaroawowede@gmail.com>
2025-08-05 11:24:03 +0100

    feat: wrapped the different routes inside the page layout file

--- frontend/KinPost/src/App.tsx
+++ frontend/KinPost/src/App.tsx
@@ -1,15 +1,20 @@
-import { Routes, Route } from 'react-router-dom'
-import './styles/globals.css'
-import SignInForm from './components/SignInForm'
-import { Home } from './navigation/pages'
+import { Routes, Route } from "react-router-dom";
+import "./styles/globals.css";
+import SignInForm from "./components/SignInForm";
+import { Home } from "./navigation/pages";
+import SignUpForm from "./components/SignUpForm";
+import PageLayout from "./navigation/PageLayout";
 
-function App() { 
+function App() {
   return (
-    <main className='flex h-screen'>
-      <Route path='/sign-in' element={< SignInForm/>}/>
-      <Route index element={<Home />}/>
+    <main className="flex h-screen">
+      <Route element={<PageLayout />}>
+        <Route path="/sign-in" element={<SignInForm />} />
+        <Route path="/sign-up" element={<SignUpForm />} />
+        <Route index element={<Home />} />
+      </Route>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
